d3d44b6f5a262ed0457c5285f87d29ddd19f4207
Chris Phillips <C_L_Phillips72@hotmail.com>
2021-10-29 19:54:42 -0500

    Week 5 project - Task 1

--- src/redux/partners.js
+++ src/redux/partners.js
@@ -1,10 +1,21 @@
-import { PARTNERS } from '../shared/partners';
+import * as ActionTypes from './ActionTypes';
 
 // This function takes in the existing "state" from the Redux store and updates that state, so the Redux store
 // has the current state.
 
-export const Partners = (state = PARTNERS, action) => {
+// This reducer responds to the 3 partner actions (ADD/LOADING/FAILED) that were added to ActionTypes.js
+export const Partners = (state = {
+    isLoading: true,
+    errMess: null,
+    partners: []
+}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_PARTNERS:
+            return {...state, isLoading: false, errMess: null, partners: action.payload};
+        case ActionTypes.PARTNERS_LOADING:
+            return {...state, isLoading: true, errMess: null, partners: []};
+        case ActionTypes.PARTNERS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
         default:
             return state;
     }
